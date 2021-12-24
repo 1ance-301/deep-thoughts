97088e715bbe81421d535aee1f26af415c015140
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-12-23 19:14:17 -0500

    added authorization

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHT } from '../utils/queries';
+import { QUERY_THOUGHT, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import FriendList from '../components/FriendList';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHT);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const loggedIn = auth.loggedIn();
 
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
@@ -12,13 +17,22 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
             <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
           )}
         </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
